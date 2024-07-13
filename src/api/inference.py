602de8a40fb8beb68fe8e7574a5b152b6705cbac
Pablo Vela <pablovela5620@gmail.com>
2024-07-12 19:57:52 -0500

    add frames_to_nerfstudio function

--- src/api/inference.py
+++ src/api/inference.py
@@ -1,5 +1,4 @@
 import torch
-import mmcv
 import numpy as np
 from pathlib import Path
 from typing import Literal, Final
@@ -7,18 +6,14 @@ from jaxtyping import Float64, Float32, UInt8
 from src.pose_utils import generate_camera_parameters
 from src.image_warping import image_depth_warping
 from src.sigma_utils import load_lambda_ts
-from src.camera_parameters import (
-    PinholeParameters,
-    rescale_intri,
-    Intrinsics,
-)
+from src.camera_parameters import PinholeParameters
+from src.nerfstudio_data import frames_to_nerfstudio
 
 from src.depth_utils import image_to_depth
 from src.rr_logging_utils import (
     log_camera,
     create_svd_blueprint,
 )
-from src.nerfstudio_data import FrameColmap, NerfStudioColmapData
 import rerun as rr
 import rerun.blueprint as rrb
 import PIL
@@ -33,149 +28,11 @@ from src.custom_diffusers_pipeline.svd import StableVideoDiffusionPipeline
 from src.custom_diffusers_pipeline.scheduler import EulerDiscreteScheduler
 
 import trimesh
-import calibur
-import json
-from dataclasses import asdict
-
-from icecream import ic
-
-DepthAnythingV2Predictor: BaseRelativePredictor = get_relative_predictor(
-    "DepthAnythingV2Predictor"
-)(device="cuda")
-SVD_PIPE = StableVideoDiffusionPipeline.from_pretrained(
-    "stabilityai/stable-video-diffusion-img2vid-xt",
-    torch_dtype=torch.float16,
-    variant="fp16",
-)
-SVD_PIPE.to("cuda")
-scheduler = EulerDiscreteScheduler.from_config(SVD_PIPE.scheduler.config)
-SVD_PIPE.scheduler = scheduler
 
 SVD_HEIGHT: Final[int] = 576
 SVD_WIDTH: Final[int] = 1024
 NEAR: Final[float] = 0.0001
 FAR: Final[float] = 500.0
-
-
-def to_nerfstudio_camera(
-    rgb_np_original: UInt8[np.ndarray, "original_h original_w 3"],
-    frames: list[PIL.Image.Image],
-    trimesh_pc_original: trimesh.PointCloud,
-    camera_list: list[PinholeParameters],
-    save_path: Path | None = None,
-) -> None:
-    assert len(frames) == len(camera_list) == 25, "Not 25 frames"
-    if save_path is not None:
-        save_path.mkdir(parents=True, exist_ok=True)
-    # send a new blueprint for nerf studio
-    parent_log_path = Path("nerfstudio")
-    rr.log(f"{parent_log_path}", rr.ViewCoordinates.LDB, static=True)
-    rr.log(
-        f"{parent_log_path}/point_cloud",
-        rr.Points3D(
-            positions=trimesh_pc_original.vertices,
-            colors=trimesh_pc_original.colors,
-        ),
-        static=True,
-    )
-
-    # resize all frames to original size
-    original_height, original_width, _ = rgb_np_original.shape
-    frames: list[PIL.Image.Image] = [
-        frame.resize(
-            (original_width, original_height),
-            PIL.Image.Resampling.BILINEAR,
-        )
-        for frame in frames
-    ]
-
-    cam_log_path = parent_log_path / "original_resolution_camera"
-    frames_colmap_list: list[FrameColmap] = []
-    for frame_id, (frame, cam_params) in enumerate(zip(frames, camera_list)):
-        rr.set_time_sequence("frame_id", frame_id)
-        original_intri: Intrinsics = rescale_intri(
-            cam_params.intrinsics,
-            target_width=original_width,
-            target_height=original_height,
-        )
-        generated_rgb_np: UInt8[np.ndarray, "h w 3"] = np.array(frame)
-        # nerfstudio uses GL convention and cam to world
-        assert cam_params.intrinsics.camera_conventions == "RDF"
-        world_T_cam_44_cv = cam_params.extrinsics.world_T_cam
-        world_T_cam_44_gl = calibur.convert_pose(
-            world_T_cam_44_cv,
-            src_convention=calibur.CC.CV,
-            dst_convention=calibur.CC.GL,
-        )
-
-        # save the generated frames
-        if save_path is not None:
-            gframe_save_path = f"{save_path}/{frame_id:06}.jpg"
-            save_bgr_img = mmcv.rgb2bgr(generated_rgb_np)
-            mmcv.imwrite(save_bgr_img, str(gframe_save_path))
-
-            frames_colmap_list.append(
-                FrameColmap(
-                    file_path=f"{frame_id:06}.jpg",
-                    transform_matrix=world_T_cam_44_gl.tolist(),
-                    colmap_im_id=frame_id,
-                )
-            )
-
-        pinhole_log_path: Path = cam_log_path / "pinhole"
-        rr.log(
-            f"{cam_log_path}",
-            rr.Transform3D(
-                mat3x3=world_T_cam_44_gl[:3, :3],
-                translation=world_T_cam_44_gl[:3, 3],
-                from_parent=False,
-            ),
-        )
-        rr.log(
-            f"{pinhole_log_path}",
-            rr.Pinhole(
-                image_from_camera=original_intri.k_matrix,
-                width=original_intri.width,
-                height=original_intri.height,
-                camera_xyz=rr.ViewCoordinates.RUB,
-            ),
-        )
-        rr.log(
-            f"{pinhole_log_path}/rgb",
-            rr.Image(generated_rgb_np).compress(jpeg_quality=80),
-        )
-
-    if save_path is not None:
-        # save point cloud
-        ply_data = trimesh_pc_original.export(file_type="ply")
-        ply_file_path = save_path / "pointcloud.ply"
-        with open(ply_file_path, "wb") as ply_file:
-            ply_file.write(ply_data)
-
-        # save the json file
-        nerf_data = NerfStudioColmapData(
-            w=original_intri.width,
-            h=original_intri.height,
-            fl_x=original_intri.fl_x,
-            fl_y=original_intri.fl_y,
-            cx=original_intri.cx,
-            cy=original_intri.cy,
-            k1=0.0,
-            k2=0.0,
-            p1=0.0,
-            p2=0.0,
-            camera_model="OPENCV",
-            ply_file_path=ply_file_path.name,
-            frames=frames_colmap_list,
-        )
-        # save to json
-        # Convert the data to a dictionary
-        nerf_data_dict = asdict(nerf_data)
-        # Convert the frames list to a list of dictionaries
-        nerf_data_dict["frames"] = [asdict(frame) for frame in nerf_data.frames]
-
-        with open(save_path / "transforms.json", "w") as f:
-            json.dump(nerf_data_dict, f)
 
 
 def nvs_solver_inference(
@@ -189,6 +46,19 @@ def nvs_solver_inference(
     num_frames: int = 25,  # StableDiffusion Video generates 25 frames
     save_path: Path | None = None,
 ) -> None:
+    # Load DepthAnythingV2Predictor and StableVideoDiffusionPipeline
+    DepthAnythingV2Predictor: BaseRelativePredictor = get_relative_predictor(
+        "DepthAnythingV2Predictor"
+    )(device="cuda")
+    SVD_PIPE = StableVideoDiffusionPipeline.from_pretrained(
+        "stabilityai/stable-video-diffusion-img2vid-xt",
+        torch_dtype=torch.float16,
+        variant="fp16",
+    )
+    SVD_PIPE.to("cuda")
+    scheduler = EulerDiscreteScheduler.from_config(SVD_PIPE.scheduler.config)
+    SVD_PIPE.scheduler = scheduler
+
     image_path: Path = Path(image_path) if isinstance(image_path, str) else image_path
     assert image_path.exists(), f"Image file not found: {image_path}"
 
@@ -296,6 +166,6 @@ def nvs_solver_inference(
         generated_rgb_np: UInt8[np.ndarray, "h w 3"] = np.array(frame)
         log_camera(cam_log_path, cam_pararms, generated_rgb_np, depth=None)
 
-    to_nerfstudio_camera(
+    frames_to_nerfstudio(
         rgb_np_original, frames, trimesh_pc_original, camera_list, save_path
     )
--- src/nerfstudio_data.py
+++ src/nerfstudio_data.py
@@ -1,5 +1,21 @@
 import numpy as np
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, asdict
+import json
+from pathlib import Path
+import PIL
+import PIL.Image
+import trimesh
+import calibur
+import mmcv
+from jaxtyping import UInt8
+from src.camera_parameters import (
+    PinholeParameters,
+    Intrinsics,
+    Extrinsics,
+    rescale_intri,
+)
+import rerun as rr
+from src.rr_logging_utils import log_camera
 
 
 @dataclass
@@ -27,3 +43,113 @@ class NerfStudioColmapData:
     applied_transform: list[list[float]] = field(
         default_factory=lambda: [[0.0] * 4 for _ in range(3)]
     )
+
+
+def frames_to_nerfstudio(
+    rgb_np_original: UInt8[np.ndarray, "original_h original_w 3"],
+    frames: list[PIL.Image.Image],
+    trimesh_pc_original: trimesh.PointCloud,
+    camera_list: list[PinholeParameters],
+    save_path: Path | None = None,
+) -> None:
+    assert len(frames) == len(camera_list) == 25, "Not 25 frames"
+    if save_path is not None:
+        save_path.mkdir(parents=True, exist_ok=True)
+    # create a new path for the final images/cameras
+    parent_log_path = Path("nerfstudio")
+    rr.log(f"{parent_log_path}", rr.ViewCoordinates.LDB, static=True)
+    rr.log(
+        f"{parent_log_path}/point_cloud",
+        rr.Points3D(
+            positions=trimesh_pc_original.vertices,
+            colors=trimesh_pc_original.colors,
+        ),
+        static=True,
+    )
+
+    # resize all frames to original size
+    original_height, original_width, _ = rgb_np_original.shape
+    frames: list[PIL.Image.Image] = [
+        frame.resize(
+            (original_width, original_height),
+            PIL.Image.Resampling.BILINEAR,
+        )
+        for frame in frames
+    ]
+
+    cam_log_path = parent_log_path / "original_resolution_camera"
+    frames_colmap_list: list[FrameColmap] = []
+    for frame_id, (frame, cam_params) in enumerate(zip(frames, camera_list)):
+        rr.set_time_sequence("frame_id", frame_id)
+        original_intri: Intrinsics = rescale_intri(
+            cam_params.intrinsics,
+            target_width=original_width,
+            target_height=original_height,
+        )
+        generated_rgb_np: UInt8[np.ndarray, "h w 3"] = np.array(frame)
+        # nerfstudio uses GL convention and cam to world
+        assert cam_params.intrinsics.camera_conventions == "RDF"
+        world_T_cam_44_cv = cam_params.extrinsics.world_T_cam
+        world_T_cam_44_gl = calibur.convert_pose(
+            world_T_cam_44_cv,
+            src_convention=calibur.CC.CV,
+            dst_convention=calibur.CC.GL,
+        )
+        original_intri.camera_conventions = "RUB"
+        extrinsics_gl = Extrinsics(
+            world_R_cam=world_T_cam_44_gl[:3, :3],
+            world_t_cam=world_T_cam_44_gl[:3, 3],
+        )
+
+        log_camera(
+            cam_log_path,
+            PinholeParameters(
+                name=str(frame_id), extrinsics=extrinsics_gl, intrinsics=original_intri
+            ),
+            generated_rgb_np,
+        )
+
+        # save the generated frames
+        if save_path is not None:
+            gframe_save_path = f"{save_path}/{frame_id:06}.jpg"
+            save_bgr_img = mmcv.rgb2bgr(generated_rgb_np)
+            mmcv.imwrite(save_bgr_img, str(gframe_save_path))
+
+            frames_colmap_list.append(
+                FrameColmap(
+                    file_path=f"{frame_id:06}.jpg",
+                    transform_matrix=world_T_cam_44_gl.tolist(),
+                    colmap_im_id=frame_id,
+                )
+            )
+
+    if save_path is not None:
+        # save point cloud
+        ply_data = trimesh_pc_original.export(file_type="ply")
+        ply_file_path = save_path / "pointcloud.ply"
+        with open(ply_file_path, "wb") as ply_file:
+            ply_file.write(ply_data)
+
+        # save the json file
+        nerf_data = NerfStudioColmapData(
+            w=original_intri.width,
+            h=original_intri.height,
+            fl_x=original_intri.fl_x,
+            fl_y=original_intri.fl_y,
+            cx=original_intri.cx,
+            cy=original_intri.cy,
+            k1=0.0,
+            k2=0.0,
+            p1=0.0,
+            p2=0.0,
+            camera_model="OPENCV",
+            ply_file_path=ply_file_path.name,
+            frames=frames_colmap_list,
+        )
+        # Convert the data to a dictionary
+        nerf_data_dict = asdict(nerf_data)
+        # Convert the frames list to a list of dictionaries
+        nerf_data_dict["frames"] = [asdict(frame) for frame in nerf_data.frames]
+
+        with open(save_path / "transforms.json", "w") as f:
+            json.dump(nerf_data_dict, f)
